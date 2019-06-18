a03cf0f7fe6e48a1bde80903d51f3b4be0adafa7
RomanBikk <dallas-113@tut.by>
2019-06-18 21:09:46 +0200

    Выведены все четные числа в диапазоне от -50 до 50

---
+++ src/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public static void main(String[] args) {
+        for (int i = -50; i<=50; i++){
+            if(i%2==0 && i!=0){
+                System.out.println(i);
+            }
+        }
+    }
+}
